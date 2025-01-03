714ab59e47c9232f5455abbad7aa50818da7f91f
aditiaariq <aditiaariq@gmail.com>
2025-01-03 11:04:18 +0700

    perbaikan footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RECALM</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         @vite('resources/sass/app.scss')
@@ -32,7 +33,7 @@
                         @endif
                     @else
                         <li class="nav-item dropdown fw-bold">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle bi-person-circle me-1" href="#"
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle  me-1" href="#"
                                 role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 <img src="{{ Auth::user()->photo ? asset('storage/' . Auth::user()->photo) : asset('images/default_profile.png') }}"
                                 alt="Profile photo"
@@ -161,22 +162,23 @@
     </div>
     <!-- Footer -->
     <footer id="gray" class="bg-primary text-white text-center py-4">
-        <div class="container" style="height: 7vh;">
-            <p class="mb-1">&copy; 2024 RECALM. All Rights Reserved.</p>
-            <ul class="list-inline mb-0">
-                <li class="list-inline-item"><a href="#" class="text-white text-decoration-none">Privacy Policy</a></li>
-                <li class="list-inline-item">|</li>
-                <li class="list-inline-item"><a href="#" class="text-white text-decoration-none">Terms of Service</a></li>
-                <li class="list-inline-item">|</li>
-                <li class="list-inline-item"><a href="#" class="text-white text-decoration-none">Contact Us</a></li>
-            </ul>
-            <div class="mt-3">
-                <a href="#" class="text-white me-3"><i class="bi bi-facebook"></i></a>
-                <a href="#" class="text-white me-3"><i class="bi bi-twitter"></i></a>
-                <a href="#" class="text-white"><i class="bi bi-instagram"></i></a>
-            </div>
+    <div class="container" style="height: 7vh;">
+        <p class="mb-1">&copy; 2024 RECALM. All Rights Reserved.</p>
+        <ul class="list-inline mb-0">
+            <li class="list-inline-item"><a href="#" class="text-white text-decoration-none">Privacy Policy</a></li>
+            <li class="list-inline-item">|</li>
+            <li class="list-inline-item"><a href="#" class="text-white text-decoration-none">Terms of Service</a></li>
+            <li class="list-inline-item">|</li>
+            <li class="list-inline-item"><a href="#" class="text-white text-decoration-none">Contact Us</a></li>
+        </ul>
+        <div class="mt-3">
+            <!-- Social Media Icons -->
+            <a href="#" class="text-white me-3"><i class="bi bi-facebook" style="font-size: 1.5rem;"></i></a>
+            <a href="#" class="text-white me-3"><i class="bi bi-twitter" style="font-size: 1.5rem;"></i></a>
+            <a href="#" class="text-white"><i class="bi bi-instagram" style="font-size: 1.5rem;"></i></a>
         </div>
-    </footer>
+    </div>
+</footer>
 
     <!-- Konten end -->
 
